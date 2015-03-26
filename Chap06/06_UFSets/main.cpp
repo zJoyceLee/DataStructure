31685e5df698d512ab02ea6f32ee81196bb86850
JoyceLee <liyuzhe0309@163.com>
2015-03-26 10:47:10 +0800

    Chap06:lab06:UFsets:assert_eq_vector.

--- Chap06/06_UFSets/main.cpp
+++ Chap06/06_UFSets/main.cpp
@@ -4,6 +4,8 @@
 //Current-Project
 #include "UFSetsNode.hpp"
 #include "UFSets.hpp"
+// Current-Test
+#include "helper.hpp"
 //g-test
 #include <gtest/gtest.h>
 
@@ -11,7 +13,9 @@ TEST(TestUFSetsClass, testUnionSet) {
     UFSets<int> s1({1}), s2({2}),   s3({3}),   s4({4}),   s5({5}),   s6({6}),   s7({7}),   s8({8});
     UFSets<int> s9({9}), s10({10}), s11({11}), s12({12}), s13({13}), s14({14}), s15({15}), s16({16});
     EXPECT_EQ(1,1);
-    EXPECT_EQ({{1, -2},  {2, 0}}, 0);
+    //EXPECT_EQ({{1, -2},  {2, 0}}, 0);
+    //assert_eq_vector({{1,-2}, {2,0}},s1.unionSet(s2));
+    assert_eq_vector({{1,-2}, {2,0}}, {{1,-2}, {2,0}});
 
 }
 
