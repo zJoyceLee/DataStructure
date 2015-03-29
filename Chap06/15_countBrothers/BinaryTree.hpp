ea7695700c7a1965bc5e9eb5600ef00dfba50c11
JoyceLee <liyuzhe0309@163.com>
2015-03-30 02:40:25 +0800

    Chap06:lab15:first commit countBrothers based on binary-tree.

---
+++ Chap06/15_countBrothers/BinaryTree.hpp
@@ -0,0 +1,76 @@
+#pragma once
+//STL
+#include <vector>
+//Current Project
+#include "LinkQueue.hpp"
+#include "BinTreeNode.hpp"
+
+template <typename T>
+class BinaryTree {
+private:
+	BinTreeNode<T> * root;
+
+	BinTreeNode<T> * CopyTree(BinTreeNode<T> * t);
+
+	void Destroy(BinTreeNode<T> * & r);
+	void PreOrder(BinTreeNode<T> * r,void(*Visit)(const T &)) const;
+
+	void InOrder(BinTreeNode<T> * r,void (*Visit)(const T &)) const;
+
+	void PostOrder(BinTreeNode<T> * r, void (*Visit)(const T &)) const;
+    int Height(const BinTreeNode<T> * r) const;
+
+	int NodeCount(const BinTreeNode<T> * r) const;
+
+	BinTreeNode<T> * Parent(BinTreeNode<T> * r, const BinTreeNode<T> * p) const;
+
+public:
+	BinaryTree();
+	BinaryTree(const T & e);
+	~BinaryTree();
+	BinTreeNode<T> * GetRoot() const;
+	bool IsEmpty() const;
+	Status GetElem(BinTreeNode<T> *p, T &e) const;
+
+	Status SetElem(BinTreeNode<T> *p, const T &e);
+
+	void InOrder(void (*Visit)(const T &)) const;
+	void PreOrder(void (*Visit)(const T &)) const;
+	void PostOrder(void (*Visit)(const T &)) const;
+	void LevelOrder(void (*Visit)(const T &)) const;
+	int NodeCount() const;
+	BinTreeNode<T> *LeftChild(const BinTreeNode<T> *p) const;
+
+	BinTreeNode<T> *RightChild(const BinTreeNode<T> *p) const;
+
+	BinTreeNode<T> *LeftSibling(const BinTreeNode<T> *p) const;
+
+	BinTreeNode<T>*RightSibling(const BinTreeNode<T>*p) const;
+
+	BinTreeNode<T> *Parent(const BinTreeNode<T> *p) const;
+
+	BinTreeNode<T> *Find(const T & e) const;
+
+	void InsertLeftChild(BinTreeNode<T> * p, const T & e);
+
+	void InsertRightChild(BinTreeNode<T> * p, const T & e);
+
+	void DeleteLeftChild(BinTreeNode<T> * p);
+	void DeleteRightChild(BinTreeNode<T> * p);
+	int	Height() const;
+	BinaryTree(const BinaryTree<T> & t);
+	BinaryTree(BinTreeNode<T> * r);
+	BinaryTree<T> & operator=(const BinaryTree<T> & t);
+//--------------------------------------------------------------------
+    std::size_t countBrothers(const std::vector<T> & vec);
+
+};
+
+template <typename T>
+void CreateBinaryTree(BinTreeNode<T> * & r, std::vector<T> pre, std::vector<T> in,
+	int preLeft, int preRight, int inLeft, int inRight);
+
+template <typename T>
+BinaryTree<T> & CreateBinaryTree(std::vector<T> pre, std::vector<T> in, int n);
+
+#include "BinaryTree.tcc"
---
+++ Chap06/15_countBrothers/main.cpp
@@ -0,0 +1,45 @@
+//STL
+#include <iostream>
+#include <vector>
+//Current Project
+#include "BinaryTree.hpp"
+//g-test
+#include <gtest/gtest.h>
+
+TEST(TestBinaryTree, TestCountBrother) {
+    //tree: a(bc(fg(h(ijk)))de)
+	std::vector<char> pre0 = {'a', 'b', 'c', 'f', 'g', 'h', 'i', 'j', 'k', 'd', 'e'};
+    std::vector<char>  in0 = {'b', 'f', 'i', 'j', 'k', 'h', 'g', 'c', 'd', 'e', 'a'};
+	BinaryTree<char> bt0 = CreateBinaryTree(pre0, in0, pre0.size());
+    //assert:post = {'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'}
+    EXPECT_EQ(4, bt0.countBrothers(in0));
+
+    //tree: 1(2(3(4)))
+    std::vector<int> pre1 = {1, 2, 3, 4};
+    std::vector<int>  in1 = {4, 3, 2, 1};
+    BinaryTree<int> bt1 = CreateBinaryTree(pre1, in1, pre1.size());
+    //post = {4, 3, 2, 1}
+    EXPECT_EQ(1, bt1.countBrothers(in1));
+
+    //tree: 1(23456)
+    std::vector<int> pre2 = {1, 2, 3, 4, 5, 6};
+    std::vector<int>  in2 = {2, 3, 4, 5, 6, 1};
+    BinaryTree<int> bt2 = CreateBinaryTree(pre2, in2, pre2.size());
+    //post = {6, 5, 4, 3, 2, 1}
+    EXPECT_EQ(5, bt2.countBrothers(in2));
+
+    //woods:: tree:1(2(4)5(8)9), tree:3, tree:6, tree:7
+    std::vector<int> pre3 = {1, 2, 4, 5, 8, 9, 3, 6, 7};
+    std::vector<int>  in3 = {4, 8, 9, 5, 2, 1, 3, 6, 7};
+    BinaryTree<int> bt3 = CreateBinaryTree(pre3, in3, pre3.size());
+    //post = {4, 8, 9, 5, 2, 7, 6, 3, 1}
+    EXPECT_EQ(3, bt3.countBrothers(in3));
+
+}
+
+int main(int argc, char * argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
