7f091c2a198e22dd7ed27f41ee44afee844eaaf0
JoyceLee <liyuzhe0309@163.com>
2015-05-16 19:59:18 +0800

    Chap09_Sort:lab05BubbleSort:add some conditions, and SegmentationFault.

--- Chap09_Sort/lab05_bubbleSortOnList/main.cpp
+++ Chap09_Sort/lab05_bubbleSortOnList/main.cpp
@@ -21,17 +21,15 @@ LinkedList<T> bubbleSort(LinkedList<T> lst) {
             if((cur->data()) > (cur->next()->data())) {
 //                std::swap(cur->data(), cur->next()->data());
 
-//                // both nodes are between begin and end
-//                // second node at the end
+                // both nodes are between begin and end
+                // second node at the end
+                // third node at the begin
+                // only two nodes
                 if(cur->prev() != nullptr && cur->next()->next() != nullptr) {
                     auto cur_tmp = cur;
-                    //std::cout << "b: " << cur->data() << std::endl;
                     auto tmp = cur->next();
-                    //std::cout << "c: " <<  tmp->data() << std::endl;
                     auto prev_tmp = cur->prev();
-                    //std::cout << "a: " << prev_tmp->data() << std::endl;
                     auto next_tmp = tmp->next();
-                    //std::cout << "d: " << next_tmp->data() << std::endl;
                     prev_tmp->next() = tmp;
                     tmp->next() = cur_tmp;
                     cur_tmp->next() = next_tmp;
@@ -67,6 +65,16 @@ LinkedList<T> bubbleSort(LinkedList<T> lst) {
                     lst.head() = tmp;
                     std::cout << "begin: ";
                     printLST(lst);
+                } else if(cur->prev() == nullptr && cur->next() != nullptr && cur->next()->next() == nullptr) {
+                    auto cur_tmp = cur;
+                    auto tmp = cur->next();
+                    tmp->next() = cur_tmp;
+                    cur_tmp->next() = nullptr;
+                    cur->prev() = tmp;
+                    tmp->prev() = nullptr;
+                    cur = tmp;
+                    lst.head() = tmp;
+                    printLST(lst);
                 }
 
             }
@@ -75,9 +83,34 @@ LinkedList<T> bubbleSort(LinkedList<T> lst) {
     return lst;
 }
 
-TEST(BubbleSort, testSort1) {
+//TEST(BubbleSort, testSortNull) {
+//    LinkedList<int> lst();
+//    printLST(bubbleSort(lst));
+//}
+
+TEST(BubbleSort, testSortMoreThanThreeNodes) {
     LinkedList<int> lst({5,7,3,2,8,4,1,0});
     //bubbleSort(lst);
+    printLST(bubbleSort(lst));
+}
+
+TEST(BubbleSort, teatSortOneNode) {
+    LinkedList<int> lst({1});
+    printLST(bubbleSort(lst));
+}
+
+TEST(BubbleSort, teatSortThreeNodes1) {
+    LinkedList<int> lst({92,1,3});
+    printLST(bubbleSort(lst));
+}
+
+TEST(BubbleSort, teatSortThreeNodes2) {
+    LinkedList<int> lst({92,1,0});
+    printLST(bubbleSort(lst));
+}
+
+TEST(BubbleSort, testSortTwoNodes) {
+    LinkedList<int> lst({99,1});
     printLST(bubbleSort(lst));
 }
 
