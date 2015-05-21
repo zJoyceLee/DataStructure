b007f0047bb783f390adaa9591abe7d6a26f6751
JoyceLee <liyuzhe0309@163.com>
2015-05-21 19:39:46 +0800

    Chap09_Sort:lab10_BinaryInsertionSort:call insertOrdered.

---
+++ Chap09_Sort/lab10_binaryInsertionSort/main.cpp
@@ -0,0 +1,87 @@
+//STL
+#include <iostream>
+#include <vector>
+//Current Test
+#include "helper.hpp"
+#include "gtest/gtest.h"
+
+template <typename T>
+std::vector<T> insertOrdered(const T & value, std::vector<T> myvec) {
+    bool inserted = false;
+    for(auto it = myvec.begin(); it != myvec.end(); ++it) {
+        if(*it > value) {
+            myvec.insert(it, value);
+            inserted = true;
+            break;
+        }
+    }
+    if(!inserted) {
+        myvec.push_back(value);
+    }
+    return myvec;
+}
+
+template <typename T>
+std::vector<T> binaryInsertionSort(std::vector<T> mykeys) {
+    std::vector<T> d_smaller, d_bigger;
+    T pivot = mykeys[0];
+    d_smaller.push_back(pivot);
+    for(std::size_t i = 1; i < mykeys.size(); ++i) {
+        if(mykeys[i] > pivot) {
+            insertOrdered(mykeys[i], d_bigger);
+        } else {
+            insertOrdered(mykeys[i], d_smaller);
+        }
+    }
+    for(std::size_t i = 0; i < d_bigger.size(); ++i) {
+        d_smaller.push_back(d_bigger[i]);
+    }
+    mykeys = d_smaller;
+    return mykeys;
+}
+
+template <typename T>
+std::vector<T> insertSort(std::vector<T> myvec) {
+    for(std::size_t i = 0; i < myvec.size(); ++i) {
+        insertOrdered(myvec[i], myvec);
+    }
+    return myvec;
+}
+
+TEST(insertOrdered, testEmpty) {
+    assert_eq_vector({5}, insertOrdered(5, {}));
+}
+
+TEST(insertOrdered, testOneElement) {
+    assert_eq_vector({5,7}, insertOrdered(5, {7}));
+    assert_eq_vector({5,7}, insertOrdered(7, {5}));
+    assert_eq_vector({8,14,14,35,99}, insertOrdered(35, {8,14,14,99}));
+}
+
+TEST(insertOrdered, testTwoElements) {
+    assert_eq_vector({5,7,9}, insertOrdered(5, {7,9}));
+    assert_eq_vector({5,7,9}, insertOrdered(7, {5,9}));
+    assert_eq_vector({5,7,9}, insertOrdered(9, {5,7}));
+}
+
+TEST(insertOrdered, testManyElements) {
+    assert_eq_vector({8,14,14,35,99}, insertOrdered(35, {8,14,14,99}));
+    assert_eq_vector({8,14,14,35,99}, insertOrdered(8, {14,14,35,99}));
+    assert_eq_vector({8,14,14,35,99}, insertOrdered(14, {8,14,35,99}));
+    assert_eq_vector({8,14,14,35,99}, insertOrdered(14, {8,14,35,99}));
+    assert_eq_vector({8,14,14,35,99}, insertOrdered(99, {8,14,14,35}));
+}
+
+TEST(InsertSort, test1) {
+    assert_eq_vector({1,2,3,4,5,6,7}, insertSort(std::vector<int>{7,5,4,2,1,3,6}));
+}
+
+TEST(BinaryInsertionSort, test1) {
+    assert_eq_vector({1,2,3,4,5,6,7}, binaryInsertionSort(std::vector<int>{7,5,4,2,1,3,6}));
+}
+
+int main(int argc, char *argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
